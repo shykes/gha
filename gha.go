0be4fe5968fed223d321f4901b675b0b4acae7e3
Solomon Hykes <solomon@dagger.io>
2024-08-09 02:58:07 +0200

    Generate Github Actions configurations from Dagger pipelines
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

---
+++ gha.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"dagger/dagger-2-gha/internal/dagger"
+	"encoding/json"
+)
+
+type Workflow struct {
+	Name string            `json:"name,omitempty" yaml:"name,omitempty"`
+	On   WorkflowTriggers  `json:"on" yaml:"on"`
+	Jobs map[string]Job    `json:"jobs" yaml:"jobs"`
+	Env  map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
+}
+
+func (w Workflow) File() *dagger.File {
+	contents, err := json.Marshal(w)
+	if err != nil {
+		panic(err)
+	}
+	var filename = w.Name + ".yml"
+	return dag.
+		Directory().
+		WithNewFile(filename, string(contents)).
+		File(filename)
+}
+
+type WorkflowTriggers struct {
+	Push             *PushEvent             `json:"push,omitempty" yaml:"push,omitempty"`
+	PullRequest      *PullRequestEvent      `json:"pull_request,omitempty" yaml:"pull_request,omitempty"`
+	Schedule         []ScheduledEvent       `json:"schedule,omitempty" yaml:"schedule,omitempty"`
+	WorkflowDispatch *WorkflowDispatchEvent `json:"workflow_dispatch,omitempty" yaml:"workflow_dispatch,omitempty"`
+	// Other event types can be added here...
+}
+
+type PushEvent struct {
+	Branches []string `json:"branches,omitempty" yaml:"branches,omitempty"`
+	Tags     []string `json:"tags,omitempty" yaml:"tags,omitempty"`
+	Paths    []string `json:"paths,omitempty" yaml:"paths,omitempty"`
+}
+
+type PullRequestEvent struct {
+	Types    []string `json:"types,omitempty" yaml:"types,omitempty"`
+	Branches []string `json:"branches,omitempty" yaml:"branches,omitempty"`
+	Tags     []string `json:"tags,omitempty" yaml:"tags,omitempty"`
+	Paths    []string `json:"paths,omitempty" yaml:"paths,omitempty"`
+}
+
+type ScheduledEvent struct {
+	Cron string `json:"cron" yaml:"cron"`
+}
+
+type WorkflowDispatchEvent struct {
+	Inputs map[string]DispatchInput `json:"inputs,omitempty" yaml:"inputs,omitempty"`
+}
+
+type DispatchInput struct {
+	Description string `json:"description,omitempty" yaml:"description,omitempty"`
+	Required    bool   `json:"required,omitempty" yaml:"required,omitempty"`
+	Default     string `json:"default,omitempty" yaml:"default,omitempty"`
+}
+
+type Job struct {
+	RunsOn         string            `json:"runs-on" yaml:"runs-on"`
+	Needs          []string          `json:"needs,omitempty" yaml:"needs,omitempty"`
+	Steps          []Step            `json:"steps" yaml:"steps"`
+	Env            map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
+	Strategy       *Strategy         `json:"strategy,omitempty" yaml:"strategy,omitempty"`
+	TimeoutMinutes int               `json:"timeout-minutes,omitempty" yaml:"timeout-minutes,omitempty"`
+	// Other job-specific fields can be added here...
+}
+
+type Step struct {
+	Name           string            `json:"name,omitempty" yaml:"name,omitempty"`
+	Uses           string            `json:"uses,omitempty" yaml:"uses,omitempty"`
+	Run            string            `json:"run,omitempty" yaml:"run,omitempty"`
+	With           map[string]string `json:"with,omitempty" yaml:"with,omitempty"`
+	Env            map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
+	TimeoutMinutes int               `json:"timeout-minutes,omitempty" yaml:"timeout-minutes,omitempty"`
+	Shell          string            `json:"shell,omitempty" yaml:"shell,omitempty"`
+	// Other step-specific fields can be added here...
+}
+
+type Strategy struct {
+	Matrix      map[string][]string `json:"matrix,omitempty" yaml:"matrix,omitempty"`
+	MaxParallel int                 `json:"max-parallel,omitempty" yaml:"max-parallel,omitempty"`
+	FailFast    bool                `json:"fail-fast,omitempty" yaml:"fail-fast,omitempty"`
+}
---
+++ main.go
@@ -0,0 +1,120 @@
+// Generate Github Actions configurations from Dagger pipelines
+package main
+
+import (
+	"dagger/dagger-2-gha/internal/dagger"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func New(
+	// Public Dagger Cloud token, for open-source projects. DO NOT PASS YOUR PRIVATE DAGGER CLOUD TOKEN!
+	// This is for a special "public" token which can safely be shared publicly.
+	// To get one, contact support@dagger.io
+	// +optional
+	publicToken string,
+	// Dagger version to run in the Github Actions pipelines
+	// +default="latest"
+	daggerVersion string,
+) *Dagger2Gha {
+	return &Dagger2Gha{
+		PublicToken:   publicToken,
+		DaggerVersion: daggerVersion,
+	}
+}
+
+type Dagger2Gha struct {
+	// +private
+	Pipelines []Pipeline
+	// +private
+	PublicToken string
+	// +private
+	DaggerVersion string
+}
+
+func (m *Dagger2Gha) OnPullRequest(
+	// +default="main"
+	branch string,
+	// +default="."
+	module string,
+	function string,
+	// +optional
+	args []string,
+	// +optional
+	noCheckout bool,
+) *Dagger2Gha {
+	m.Pipelines = append(m.Pipelines, Pipeline{
+		OnPullRequestBranch: branch,
+		Module:              module,
+		Function:            function,
+		Args:                args,
+		Checkout:            !noCheckout,
+		DaggerVersion:       m.DaggerVersion,
+	})
+	return m
+}
+
+func (m *Dagger2Gha) Config() *dagger.Directory {
+	dir := dag.Directory()
+	for i, pipeline := range m.Pipelines {
+		dir = dir.WithFile(
+			fmt.Sprintf(".github/workflows/%d.yml", i+1),
+			pipeline.Config().File(),
+		)
+	}
+	return dir
+}
+
+type Pipeline struct {
+	OnPullRequestBranch string
+	Module              string
+	Function            string
+	Args                []string
+	Checkout            bool
+	DaggerVersion       string
+}
+
+func (p *Pipeline) Name() string {
+	return strings.Join(append([]string{p.Function}, p.Args...), " ")
+}
+
+func (p *Pipeline) Config() Workflow {
+	var steps []Step
+	if p.Checkout {
+		steps = append(steps, Step{
+			Name: "Checkout",
+			Uses: "actions/checkout@v4",
+		})
+	}
+	steps = append(steps, Step{
+		Name: "Call Dagger",
+		Uses: "dagger/dagger-for-github@v6",
+		With: map[string]string{
+			"version": "latest",
+			"module":  p.Module,
+			"args":    shellCommand(append([]string{p.Function}, p.Args...)),
+		},
+	})
+	return Workflow{
+		Name: p.Name(),
+		On: WorkflowTriggers{
+			PullRequest: &PullRequestEvent{
+				Branches: []string{p.OnPullRequestBranch},
+			},
+		},
+		Jobs: map[string]Job{
+			"main": Job{
+				Steps: steps,
+			},
+		},
+	}
+}
+
+func shellCommand(args []string) string {
+	var escapedArgs []string
+	for _, arg := range args {
+		escapedArgs = append(escapedArgs, strconv.Quote(arg))
+	}
+	return strings.Join(escapedArgs, " ")
+}
