584a9b4ba95c3fe1bd1378edcd3106dfa115a5cb
Solomon Hykes <solomon@dagger.io>
2024-08-14 02:26:51 +0200

    Manage our own CI by daggerizing .github, genius!
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

---
+++ .github/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/shykes/gha/.github/internal/dagger"
+)
+
+type Github struct{}
+
+// Returns a container that echoes whatever string argument is provided
+func (m *Github) Generate() *dagger.Directory {
+	return dag.
+		Gha().
+		OnPush(
+			"git --url=https://github.com/$GITHUB_REPOSITORY branch --name=$GITHUB_REF tree glob --pattern=*",
+			dagger.GhaOnPushOpts{
+				Module: "github.com/shykes/core",
+			}).
+		Config().
+		Directory(".github")
+}
